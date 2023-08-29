8d9d1d7dcf71982a11993ca91d651a1bfb00a4b2
KarboXXX <renanduartebr.yahoo.com.br@gmail.com>
2023-08-29 00:59:16 -0300

    added quiz logic (not optimized)

---
+++ script.js
@@ -0,0 +1,226 @@
+const quiz = {
+    "perguntas": [
+        "O que é o sistema econômico do capitalismo e quais são seus princípios fundamentais?",
+        "Quais são as principais características distintivas do capitalismo em comparação com outros sistemas econômicos?",
+        "Como o capitalismo influencia a alocação de recursos em uma economia?",
+        "Quais são os principais argumentos a favor e contra o capitalismo como sistema econômico?",
+        "Como o livre mercado está relacionado ao capitalismo, e quais são os benefícios e desafios desse sistema",
+        "Quais são as diferenças entre o capitalismo de livre mercado e o capitalismo de estado?",
+        "De que forma a competição é incentivada no sistema capitalista e como isso pode afetar os preços e a qualidade dos produtos?",
+        "Quais são os papéis do governo em uma economia capitalista? Até que ponto o governo deve intervir?",
+        "Como o capitalismo impacta a desigualdade econômica e a distribuição de renda dentro de uma sociedade?",
+        "Quais são as críticas frequentes ao capitalismo relacionadas à exploração da mão de obra e aos direitos dos trabalhadores?",
+        "Como o consumismo é impulsionado pelo capitalismo e qual é o seu impacto no meio ambiente?",
+        "Quais são os efeitos do capitalismo na inovação tecnológica e no progresso econômico?",
+        "Como o capitalismo influencia a globalização e as relações econômicas entre os países?",
+        "Quais são os exemplos de regulamentações governamentais que foram implementadas para controlar excessos do capitalismo?",
+        "Qual é a relação entre o capitalismo e a busca incessante pelo crescimento econômico, e existem alternativas viáveis a esse modelo?"
+    ],
+
+    "respostas": [
+        "O capitalismo é um sistema econômico baseado em propriedade privada e busca de lucro. Seus princípios incluem concorrência e oferta-demanda.",
+        "O capitalismo se destaca pela propriedade privada, livre concorrência, lucro e escolha individual, diferindo de sistemas centralizados.",
+        "O capitalismo influencia recursos através de preços de mercado. Recursos vão onde há demanda e lucro, otimizando alocação.",
+        "Prós: Inovação, eficiência. Contras: Desigualdade, exploração. Debate sobre o papel regulatório do governo é central.",
+        "O livre mercado é central no capitalismo. Vantagens: eficiência, diversidade. Desafios: desigualdade, instabilidade.",
+        "Capitalismo de livre mercado tem pouca regulação, enquanto o de estado envolve mais controle estatal sobre setores-chave.",
+        "Competição no capitalismo ocorre por lucro, beneficiando preços e qualidade. Excesso pode gerar ética duvidosa e falta de inovação.",
+        "Governos em capitalismo regulam, fiscalizam e fornecem bens públicos. Extensão da intervenção varia por contexto.",
+        "Capitalismo pode agravar desigualdades devido à acumulação de riqueza. Distribuição de renda é impactada pela estrutura.",
+        "Críticas: Exploração e direitos dos trabalhadores. Salários baixos e más condições de trabalho são frequentemente debatidos.",
+        "Capitalismo incentiva consumo para crescimento econômico. Isso afeta ambiente por consumo excessivo e resíduos.",
+        "Capitalismo impulsiona inovação pela concorrência. Progresso econômico vem da busca por eficiência e novas soluções.",
+        "Capitalismo impulsiona globalização via comércio internacional. Isso traz vantagens econômicas e desafios globais.",
+        "Exemplos: Leis antitruste evitam monopólios. Regulamentações ambientais controlam impacto ecológico.",
+        "Capitalismo frequentemente valoriza crescimento econômico. Alternativas buscam equilíbrio entre desenvolvimento e bem-estar."
+      ],
+
+      "espantalho": [
+        [
+          "O capitalismo é um sistema que elimina completamente a desigualdade econômica.",
+          "No capitalismo, o governo controla todos os aspectos da produção.",
+          "No capitalismo, a oferta e a demanda não têm influência na alocação de recursos."
+        ],
+        [
+          "O capitalismo só se diferencia de outros sistemas econômicos por ter um único proprietário em cada indústria.",
+          "No capitalismo, todas as empresas são de propriedade estatal.",
+          "Capitalismo não tem relação com lucro e competição."
+        ],
+        [
+          "O capitalismo aloca recursos aleatoriamente, sem considerar a demanda.",
+          "No capitalismo, os preços são definidos pelo governo sem considerar oferta e demanda.",
+          "Capitalismo não tem impacto na alocação de recursos em uma economia."
+        ],
+        [
+          "O capitalismo não gera desigualdade econômica de forma alguma.",
+          "No capitalismo, todos têm as mesmas oportunidades, independentemente das circunstâncias.",
+          "Capitalismo não possui críticas válidas e é perfeito em todos os aspectos."
+        ],
+        [
+          "O livre mercado no capitalismo não afeta preços e não tem relação com eficiência econômica.",
+          "No capitalismo, o livre mercado implica que todas as transações são controladas pelo governo.",
+          "Livre mercado no capitalismo implica em cartelização e controle de preços."
+        ],
+        [
+          "Capitalismo de estado e capitalismo de livre mercado são exatamente iguais.",
+          "No capitalismo de estado, não há presença governamental na economia.",
+          "Capitalismo de estado e capitalismo de livre mercado não diferem em seus princípios."
+        ],
+        [
+          "No capitalismo, a competição não desempenha papel importante e as empresas colaboram sempre.",
+          "No capitalismo, os preços dos produtos não são influenciados pela concorrência.",
+          "Competição no capitalismo leva a preços altos e produtos de baixa qualidade."
+        ],
+        [
+          "No capitalismo, o governo não tem nenhum papel a desempenhar na economia.",
+          "No capitalismo, o governo controla todas as empresas de forma centralizada.",
+          "O governo no capitalismo não pode intervir em nenhum aspecto econômico."
+        ],
+        [
+          "Capitalismo não tem impacto na desigualdade econômica; todos têm renda igual.",
+          "Desigualdade econômica não é uma preocupação no capitalismo.",
+          "Capitalismo não influencia a distribuição de renda em uma sociedade."
+        ],
+        [
+          "Exploração da mão de obra não ocorre no capitalismo; todos os trabalhadores têm ótimas condições.",
+          "No capitalismo, os direitos dos trabalhadores são protegidos de maneira absoluta.",
+          "Capitalismo não apresenta desafios significativos em relação aos direitos dos trabalhadores."
+        ],
+        [
+          "No capitalismo, o consumismo não é uma questão importante, já que todos consomem de forma equilibrada.",
+          "O consumo não tem relação com o capitalismo; é um problema independente.",
+          "Consumismo não tem impacto no meio ambiente e é benéfico para a economia."
+        ],
+        [
+          "No capitalismo, não há inovação tecnológica ou progresso econômico.",
+          "Capitalismo não promove inovação; todas as ideias criativas são rejeitadas.",
+          "Inovação tecnológica é prejudicial para a economia capitalista."
+        ],
+        [
+          "Capitalismo não tem relação com a globalização e não afeta as relações econômicas entre países.",
+          "No capitalismo, os países não comercializam entre si; cada um é autossuficiente.",
+          "Globalização não tem relevância no contexto do capitalismo."
+        ],
+        [
+          "Regulamentações governamentais no capitalismo nunca são implementadas.",
+          "Capitalismo não precisa de regulamentações; se auto-regula completamente.",
+          "Regulamentações no capitalismo sempre resultam em caos econômico."
+        ],
+        [
+          "A busca incessante pelo crescimento econômico é uma característica do socialismo, não do capitalismo.",
+          "Capitalismo não está interessado em crescimento econômico.",
+          "Alternativas ao crescimento econômico no capitalismo não são possíveis."
+        ]
+      ]
+}
+
+var pos = (Math.floor(Math.random() * quiz.perguntas.length));
+var copos = (Math.floor(Math.random() * 4));
+document.addEventListener('DOMContentLoaded', () => {
+    var places = ['A', 'B', 'C', 'D'];
+    var pontos = 0;
+
+    function randomize() {
+        return [(Math.floor(Math.random() * quiz.perguntas.length)), (Math.floor(Math.random() * 4))];
+    }
+
+    function errado () {
+        console.warn('ERRADO');
+
+        pontos -= 10;
+        refreshPoints(pontos);
+
+        for (var i=0; i<places.length; i++) {
+            document.getElementById('btn-' + places[i]).removeEventListener('click', correto, true);
+            document.getElementById('btn-' + places[i]).removeEventListener('click', errado, true);
+        }
+
+        pos = (Math.floor(Math.random() * quiz.perguntas.length));
+        copos = (Math.floor(Math.random() * 4));
+
+        return replayQuiz(pos, copos);
+    }
+
+    function correto () {
+        console.info('CORRETO');
+        pontos += 10;
+        refreshPoints(pontos);
+
+        for (var i=0; i<places.length; i++) {
+            document.getElementById('btn-' + places[i]).removeEventListener('click', correto, true);
+            document.getElementById('btn-' + places[i]).removeEventListener('click', errado, true);
+        }
+
+        pos = (Math.floor(Math.random() * quiz.perguntas.length));
+        copos = (Math.floor(Math.random() * 4));
+
+        return replayQuiz(pos, copos);
+
+    }
+
+    function replayQuiz(posr, coposr) {
+        var pacedpositions = [];
+
+        var tries = 0;
+
+        document.getElementById('pergunta').innerHTML = quiz.perguntas[posr];
+        document.getElementById(places[coposr]).innerHTML = places[coposr].toString() + ") " + quiz.respostas[posr];
+        for (var i=0; i<places.length; i++) {
+            document.getElementById('btn-' + places[i]).removeEventListener('click', correto, true);
+            document.getElementById('btn-' + places[i]).removeEventListener('click', errado, true);
+            if (i != coposr) {
+                var esp = parseInt(Math.floor(Math.random() * 3));
+                if (pacedpositions.length > 0) {
+                    while (pacedpositions.includes(esp) || esp == coposr) {
+                        if (tries >= 1000) break;
+                        esp = parseInt(Math.floor(Math.random() * 3));
+                        tries++;
+                    }
+                }
+                pacedpositions[i] = esp;
+                document.getElementById(places[i]).innerHTML = places[i].toString() + ") " + quiz.espantalho[posr][esp];
+                
+            }
+            // console.log(i, copos, pacedpositions, esp, tries);
+            console.log(posr, coposr);
+            
+        }
+
+        for (var i=0; i<places.length; i++) {
+            if (i != copos) {
+                document.getElementById('btn-' + places[i]).addEventListener('click', errado, true);
+            }
+        }
+        
+        document.getElementById('btn-' + places[copos]).addEventListener('click', correto, true);
+        
+    }
+
+    function refreshPoints(new_pontos) {
+        document.getElementById('pontos').innerHTML = 'Pontuação: ' + new_pontos;
+    }
+    
+
+    pos = (Math.floor(Math.random() * quiz.perguntas.length));
+    copos = (Math.floor(Math.random() * 4));
+
+    // document.getElementById('btn-' + places[i]).addEventListener('click', () => errado(), true);
+    // document.getElementById('btn-' + places[copos]).addEventListener('click', () => correto(), true);
+    // for (var i=0; i<places.length; i++) {
+    //     if (i != copos) {
+    //         document.getElementById('btn-' + places[i]).addEventListener('click', () => errado());
+    //     }
+    // }
+    
+    // document.getElementById('btn-' + places[copos]).addEventListener('click', () => correto());
+
+    replayQuiz(pos, copos);
+    // for (var i=0; i<places.length; i++) {
+    //     if (i != copos) {
+    //         document.getElementById('btn-' + places[i]).addEventListener('click', errado, true);
+    //     }
+    // }
+    
+    // document.getElementById('btn-' + places[copos]).addEventListener('click', correto, true);
+
+})
